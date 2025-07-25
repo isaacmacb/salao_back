07c182a7890ad474ae026a944c795852ac5282f1
Isaacmacielbe <inaqtage@gmail.com>
2025-07-24 23:31:25 -0300

    inicializando o projeto

---
+++ src/main/java/com/app/salaodesobrancelhas/SalaodesobrancelhasApplication.java
@@ -0,0 +1,13 @@
+package com.app.salaodesobrancelhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaodesobrancelhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaodesobrancelhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/salaodesobrancelhas/SalaodesobrancelhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.salaodesobrancelhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaodesobrancelhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
